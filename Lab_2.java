06758519564bdeb43f901eee90020f028dd5112f
CM115abhiraw1205 <123328577+CM115abhiraw1205@users.noreply.github.com>
2023-06-01 20:39:02 -0400

    Files Upload for Lab_2

---
+++ Lab_2.java
@@ -0,0 +1,13 @@
+// Main class
+public class Lab_2 {
+    public static void main(String[] args) {
+        Rectangle R1 = new Rectangle(6, 5);
+        Square S1 = new Square(5);
+
+        System.out.println("The area of Rectangle is " + R1.getArea());
+        System.out.println("I have " + R1.getSides() + " " + "sides.");
+
+        System.out.println("The area of Square is " + S1.getArea());
+        System.out.println("I Can get sides of a Polygon.");
+    }
+}
